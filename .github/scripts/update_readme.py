33a19a407f8595f3a925951cd935b87a4cda2807
tejaswini-roopa <brvltejaswini@gmail.com>
2026-05-05 21:43:45 +0530

    05-05-2026

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -6,7 +6,7 @@ TOPICS = [
     "StackAndQueue", "Trees", "BinarySearch",
     "DynamicProgramming", "Graphs",
     "Backtracking", "Greedy",
-    "BitManipulation","ModularArithmetic","Recursion",
+    "BitManipulation","ModularArithmetic","Recursion","Sorting",
 ]
 
 DIFFICULTY = {
